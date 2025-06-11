f92f4c6b6d9806ad16c2bcbebdefcd39f443a868
Gaya9912 <gayathripolisetty59@gmail.com>
2025-06-11 22:24:07 +0530

    Add files via upload

---
+++ Python CRT/ExampleQuestion.py
@@ -0,0 +1,19 @@
+Tuple=(5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100)
+#Print the first 5 elemntse of tupole with slicing
+print(Tuple)
+print(Tuple[:5:])
+#Print the first 5 elemntse of tupole with slicing
+print(Tuple[:10:])
+#Print the first 5 elemntse of tupole with slicing
+
+#print the first 10 elements of tuple with slicing
+
+#print the last 5 elements of tuple with slicing
+
+#print the last 10 elements of tuple with slicing
+
+#print the least 3 elements of tuple with slicing
+
+#print the elements of tuple from last 5th to 10th with slicing
+
+#print the elements
---
+++ Python CRT/playlist.py
@@ -0,0 +1,8 @@
+play_List=[]
+for i in rang(10):
+    song_name=(f"Enter the Index value{i}:")
+    playList_name.append(song_name)
+print("The List of the songs are:{playlist}")
+print(playlist[::-1])
+
+
